a9ac6d4dd5b0e911595ffd07749cb0b9df2eacd4
walacefex <walacefex01@gmail.com>
2020-05-01 16:47:41 -0300

    Criando o metodo deletar e editar o cliente

--- api/cliente.js
+++ api/cliente.js
@@ -1,30 +1,52 @@
 //promises Javascript
 //GET
-const listarClientes  = ()  => {
-     return fetch('http://localhost:4000/clientes')
-    .then(resposta =>{
-        return resposta.json()
+const listarClientes = () => {
+    return fetch("http://localhost:4000/clientes", {
+      method: "get"
     })
-    .then(json =>{
-        console.log(json)
-        return json
-    })
-}
+      .then(resp => {
+        return resp.json();
+      })
+      .then(json => {
+        return json;
+      });
+  };
 
 //POST
-const cadastrarClientes = (nome, cpf) => {
-    const Json = JSON.stringify({
-    nome: nome,
-    cpf:  cpf 
-    })
-    return fetch('http://localhost:4000/clientes/cliente',{  
-        method: 'POST',
-        headers: {
-            'Content-type': 'application/json'
-        },
-        body: Json
-    })    
-    .then(respo =>{
-         return respo.body
-    })
-}
+const cadastrarCliente = (nome, cpf) => {
+    const json = JSON.stringify({
+      nome: nome,
+      cpf: cpf
+    });
+  
+    return fetch("http://localhost:4000/clientes/cliente", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: json
+    }).then(resp => {
+      
+        return resp.body;
+      } 
+    );
+  }
+
+  //Delete
+  const deletaCliente = id => {
+      return fetch(`http://localhost:4000/clientes/cliente/${id}`,{
+          method: 'DELETE'
+      })
+  }
+
+  //Editar
+
+  const detalhaCliente = id =>{
+      return fetch(`http://localhost:4000/clientes/cliente/${id}`,{
+          method: 'GET'
+      })
+
+      .then(resposta => {
+          return resposta.json();
+      })
+  }
