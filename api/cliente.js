3657668dc53d64aeac6b27bc5e5273d83b78c768
walacefex <walacefex01@gmail.com>
2020-05-01 17:09:21 -0300

    criando o metodo de detalhar o cliente e editar o cliente recebendo o seu nome, cpf e id utilizando o method PUT

--- api/cliente.js
+++ api/cliente.js
@@ -45,8 +45,22 @@ const cadastrarCliente = (nome, cpf) => {
       return fetch(`http://localhost:4000/clientes/cliente/${id}`,{
           method: 'GET'
       })
-
       .then(resposta => {
-          return resposta.json();
+          return resposta.json()
+      })
+  }
+
+  const editaCliente = (id, cpf, nome) =>{
+    const json = JSON.stringify({
+        nome: nome,
+        cpf: cpf
+      });
+    
+      return fetch(`http://localhost:4000/clientes/cliente/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: json
       })
   }
