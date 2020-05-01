32ec4b0bb30a10f2a00204a0adebafe693468d74
walacefex <walacefex01@gmail.com>
2020-05-01 17:43:24 -0300

    criando as mensagens de erro e de sucesso para se o cliente conseguiu editar o cliente ou deu erro

--- edita-cliente.js
+++ edita-cliente.js
@@ -11,7 +11,31 @@ detalhaCliente(id).then(dados =>{
     inputNome.value = dados[0].nome
 })
 
-const formEdicao = document.querySelector('[data-form]')
+const formEdicao = document.querySelector('[data-form]');
+const mensgasagemSucesso = (mensagem) => {
+    const linha = document.createElement("tr");
+
+    const conteudoLinha =`
+    <div class="alert alert-success" role="alert">
+    ${mensagem}
+    </div>
+`;
+
+    linha.innerHTML = conteudoLinha;
+    return linha;
+}
+const mensgasagemErro = (mensagem) => {
+    const linha = document.createElement("tr");
+
+    const conteudoLinha =`
+    <div class="alert alert-warning" role="alert">
+    ${mensagem}
+    </div>
+`;
+
+    linha.innerHTML = conteudoLinha;
+    return linha;
+}
 
 formEdicao.addEventListener('submit', event => {
     event.preventDefault();
@@ -20,5 +44,11 @@ formEdicao.addEventListener('submit', event => {
         alert('Esse CPF não existe');
         return
     }
-    editaCliente(id, inputCPF.value, inputNome.value)
+    editaCliente(id, inputCPF.value, inputNome.value).then(resposta => {
+        if(resposta.status === 200){
+            formEdicao.appendChild(mensgasagemSucesso('Cliente editado com sucesso! '))
+        }else{
+            formEdicao.appendChild(mensgasagemErro('Erro na edição do cliente'))
+        }
+    })
 })
